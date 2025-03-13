458a8d76528d366f33c364d722d726c9c3d22f16
subvair <vadimogienko42k@gmail.com>
2025-03-13 23:42:12 +0700

    Добавил кастомные исключения

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+public class MatrixException : Exception
+{
+    public MatrixException(string message) : base(message) { }
+}
+
+public class SingularMatrixException : MatrixException
+{
+    public SingularMatrixException(string message) : base(message) { }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
