bb0b48f15d127e2c678578e21e34afc44ee1958e
subvair <vadimogienko42k@gmail.com>
2025-03-13 23:46:41 +0700

    Реализовал базавую конструкцию для создания матрици, с возможностью автомотического заполнения рандомом

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 
-public class MatrixException : Exception
-{
+public class MatrixException : Exception {
     public MatrixException(string message) : base(message) { }
 }
 
-public class SingularMatrixException : MatrixException
-{
+public class SingularMatrixException : MatrixException {
     public SingularMatrixException(string message) : base(message) { }
+}
+
+public class SquareMatrix {
+    private double[,] _data;
+    public int Size { get; }
+
+    public SquareMatrix(int size, bool randomize = false)
+    {
+        if (size <= 0) throw new MatrixException("Размер матрицы должен быть положительным");
+        Size = size;
+        _data = new double[size, size];
+        if (randomize) FillRandom();
+    }
+
+    private void FillRandom()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < Size; i++)
+            for (int j = 0; j < Size; j++)
+                _data[i, j] = rand.NextDouble() * 10;
+    }
+
 }
 
 
